07ea65bb9b2b150f25f1d6f6df85682a26643351
João Layon <104402531+DuJao22@users.noreply.github.com>
2025-02-19 19:25:08 -0300

    Atualizar o gerar_etiqueta_geral.php

--- templates/gerar_etiqueta_geral.php
+++ templates/gerar_etiqueta_geral.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport">
+    <meta name="viewport" >
     <title>Gerador de Etiquetas | 3D Lab</title>
     <script src="https://cdn.jsdelivr.net/npm/jsbarcode@3.11.0/dist/JsBarcode.all.min.js"></script>
     <script>
@@ -16,7 +16,7 @@
                 format: "CODE128",
                 displayValue: false,
                 height: 22,
-                fontSize: 8,
+                fontSize: 15,
                 margin: 0,
                 fontOptions: "bold"
             });
@@ -47,18 +47,19 @@
             align-items: center;
             height: 90vh;
             width: 125%;
+            
         }
 
         .etiqueta {
-            width: {{ produto.comprimento_etiqueta }}cm;
-            height: {{ produto.altura_etiqueta }}cm;
+            width: {{ comprimento_etiqueta }}px; /* Utilizando a configuração escolhida */
+            height: {{ altura_etiqueta }}px; /* Utilizando a configuração escolhida */
             transform: rotate(-90deg);
             position: center;
         }
 
         .etiqueta2 {
-            width: {{ produto.comprimento_etiqueta }}cm;
-            height: {{ produto.altura_etiqueta }}cm;
+            width: {{ comprimento_etiqueta }}px; /* Utilizando a configuração escolhida */
+            height: {{ altura_etiqueta }}px; /* Utilizando a configuração escolhida */
             transform: rotate(-90deg);
         }
 
@@ -67,6 +68,10 @@
             width: 100%;
             height: 50%;
         }
+
+        .text {
+            font-size: {{ tamanho_texto }}px; /* Utilizando a configuração escolhida */
+        }
     </style>
 </head>
 <body onload="ClosePrint()">
@@ -74,20 +79,21 @@
     <div class="etiqueta">
         <center>
             <p></p>
-            <span style="font-family:arial;font-size:15px;"><strong>{{ produto.material }}</strong></span><br/>
-            <span style="font-family:arial;font-size:15px;"><strong>{{ produto.cor }}</strong></span><br/>
-            <span style="font-family:arial;font-size:14px;text-transform:uppercase;">{{ produto.descricao }}</span>
+            <span class="text" style="font-family:arial;"><strong>{{ produto.material }}</strong></span><br/>
+            <span class="text" style="font-family:arial;"><strong>{{ produto.cor }}</strong></span><br/>
+            <span class="text" style="font-family:arial;text-transform:uppercase;">{{ produto.descricao }}</span>
             <svg class="barcode">
             </svg>
-            <span style="font-family:arial;font-size:13px;"><strong>{{ produto.codigo }}</strong></span>
+            <span class="text" style="font-family:arial;"><strong>{{ produto.codigo }}</strong></span>
         </center>
     </div>
     <div class="etiqueta2">
         <center>
-            <span style="font-family:arial;font-size:15px;"><strong>{{ produto.material }}</strong></span><br/>
-            <span style="font-family:arial;font-size:15px;"><strong>{{ produto.cor }}</strong></span><br/>
-            <span style="font-family:arial;font-size:14px;text-transform:uppercase;">{{ produto.descricao }}</span><br/>
-            <span style="font-family:arial;font-size:17px;text-transform:uppercase;"><small>{{ produto.informacoes_adicionais }}</small></span>
+            
+            <span class="text" style="font-family:arial;"><strong>{{ produto.material }}</strong></span><br/>
+            <span class="text" style="font-family:arial;"><strong>{{ produto.cor }}</strong></span><br/>
+            <span class="text" style="font-family:arial;text-transform:uppercase;">{{ produto.descricao }}</span><br/>
+            <span class="text" style="font-family:arial;text-transform:uppercase;"><small>{{ produto.informacoes_adicionais }}</small></span>
         </center>
     </div>
 </div>
